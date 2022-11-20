b44b412fa094d86b275ecc0f41199ae78f9c70cf
Janaina Bacin <janabacrauber@gmail.com>
2022-11-20 09:05:15 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
-  return(
+  return (
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton />
+    </>
   )
 }
 
